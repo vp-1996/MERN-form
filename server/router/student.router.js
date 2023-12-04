7c1e51732eaeff5b82f6ba928739716077feb6a7
vp-1996 <119441482+vp-1996@users.noreply.github.com>
2023-12-04 19:43:19 +0530

    Delete server directory

--- server/router/student.router.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { addStudent } from '../controller/student.cont'
-
-let studentRouter = express.Router()
-
-studentRouter.post('/addStudent',addStudent)
-
-export default studentRouter
